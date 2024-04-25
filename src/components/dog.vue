6b4529ff9090d42d22da98a6f700a74154772a2f
liushijia <3020422083@qq.com>
2024-04-25 22:11:57 +0800

    初始setup语法糖，和ref响应式数据基础

--- src/components/dog.vue
+++ src/components/dog.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="dog">
     <h1>狗</h1>
-    <h2>姓名：{{name}}</h2>
-    <h2>年龄：{{age}}</h2>
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
     <button @click="brak">狗叫</button>
+    <button @click="changeName">变身</button>
+    <button @click="changeAge">增加年龄</button>
   </div>
 
 </template>
 
-<script lang="ts">
-export default {
-  name: 'Dog',
-  setup (){
-    // 数据
-    let name = "大黄";
-    let age: number = 3;
+<script lang="ts" setup name="Dog">
+// 引入 ref
+import {ref} from 'vue'
 
-    // 方法
-    function brak(){
-      alert(name + "在叫");
-    }
+// 数据
+let name = ref("大黄");
+let age = ref(2);
 
-
-    return {name, age, brak}
-  }
+// 方法
+function changeName() {
+  if (name.value === "大黄")
+    name.value = "孙一楠";
+  else name.value = "大黄";
 }
+
+function changeAge() {
+  age.value += 1;
+}
+
+function brak() {
+  alert(name.value + "正在狗叫，点击修改姓名直接变身");
+}
+
 </script>
 
 <style>
-.dog{
+.dog {
   background-color: skyblue;
   box-shadow: 0 0 10px;
   border-radius: 10px;
--- src/components/person.vue
+++ src/components/person.vue
@@ -3,6 +3,7 @@
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
+    <h2>地址：{{address}}</h2>
     <button @click="changeName">修改姓名</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
@@ -10,35 +11,29 @@
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'Person',
-  setup(){
-    // console.log(this)   setup中的this是undefined，vue3中已经开始弱化this了
-    // 数据
-    let name: string = "张三";   // 此时的name不是响应式的
-    let age: number = 10;   // 此时的age不是响应式的
-    let tel: string = "1231231";  // 此时的tel也不是响应式的
 
-    // 方法
-    function changeName (){  // setup中没有维护this关键字，不用使用this关键字
-      if (name === "张三")
-        name = "zhang_san";
-      else name = "张三";
-    }
+<script lang="ts" setup name="person">
+// 引入ref
+import {ref} from 'vue'
+// 数据
+let name = ref("tigood");
+let age = ref(18);
+let tel: string = "1231231";
+let address: string = "华北理工大学轻工学院";
 
-    function changeAge(){
-      age += 1;
-    }
-
-    function showTel(){
-      alert(tel);
-    }
-
-
-    return {name, age, changeAge, changeName, showTel}  // 应该是{name: name, age: age} 不过这里触发了js的对象简写 可以简写为这样
-  }
+// 方法
+function changeName (){
+  if (name.value === "刘时佳")
+    name.value = "tigood";
+  else name.value = "刘时佳";
 }
+function showTel(){
+  alert(tel);
+}
+function changeAge(){
+  age.value += 1;
+}
+
 </script>
 
 <style scoped>
