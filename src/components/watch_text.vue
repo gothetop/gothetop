161ea7502445f32cb031a7b8f4c7abc2827e4d36
liushijia <3020422083@qq.com>
2024-04-28 21:57:28 +0800

    随手交一下

---
+++ src/components/watch_text.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts" name="WatchText">
+import {ref} from "vue";
+
+// 数据
+
+</script>
+
+<template>
+<div class="watch_text">
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
